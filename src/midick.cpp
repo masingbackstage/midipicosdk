47dca54fcb02f8b1e55d6436e0df522d83027e2e
masingbackstage <filipszon123@gmail.com>
2024-09-30 03:24:04 +0200

    Added simple tinyusb midi implementation

--- src/midick.cpp
+++ src/midick.cpp
@@ -1,14 +1,79 @@
 #include "pico/stdlib.h"
 #include "../include/button.h"
+#include "tusb.h"
+#include "bsp/board.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+
+void midi_task(void);
 
 int main()
 {
-    stdio_init_all();
 
+    stdio_init_all();
+    
+    board_init();
+
+    tusb_init();
 
 
     Button button1(5,14,15);
     while (true) {
         button1.update();
+        tud_task(); // tinyusb device task
+        midi_task();
     }
+}
+
+
+
+
+//--------------------------------------------------------------------+
+// MIDI Task
+//--------------------------------------------------------------------+
+
+// Variable that holds the current position in the sequence.
+uint32_t note_pos = 0;
+
+// Store example melody as an array of note values
+uint8_t note_sequence[] =
+{
+  74,78,81,86,90,93,98,102,57,61,66,69,73,78,81,85,88,92,97,100,97,92,88,85,81,78,
+  74,69,66,62,57,62,66,69,74,78,81,86,90,93,97,102,97,93,90,85,81,78,73,68,64,61,
+  56,61,64,68,74,78,81,86,90,93,98,102
+};
+
+void midi_task(void)
+{
+  static uint32_t start_ms = 0;
+  uint8_t msg[3];
+
+  // send note every 1000 ms
+  if (board_millis() - start_ms < 286) return; // not enough time
+  start_ms += 286;
+
+  // Previous positions in the note sequence.
+  int previous = note_pos - 1;
+
+  // If we currently are at position 0, set the
+  // previous position to the last note in the sequence.
+  if (previous < 0) previous = sizeof(note_sequence) - 1;
+
+  // Send Note On for current position at full velocity (127) on channel 1.
+  msg[0] = 0x90;                    // Note On - Channel 1
+  msg[1] = note_sequence[note_pos]; // Note Number
+  msg[2] = 127;                     // Velocity
+  tud_midi_n_stream_write(0, 0, msg, 3);
+
+  // Send Note Off for previous note.
+  msg[0] = 0x80;                    // Note Off - Channel 1
+  msg[1] = note_sequence[previous]; // Note Number
+  msg[2] = 0;                       // Velocity
+  tud_midi_n_stream_write(0, 0, msg, 3);
+
+  // Increment position
+  note_pos++;
+
+  // If we are at the end of the sequence, start over.
+  if (note_pos >= sizeof(note_sequence)) note_pos = 0;
 }
